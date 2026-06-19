6e2fe2ffaa035230586958a9bc65732e654c75a6
himrshafiee <himrshafiee@gmail.com>
2026-06-19 22:11:07 +0200

    create Places project app

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Amin Shafiee on 19/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by Amin Shafiee on 19/06/2026.
+//
+
+import Testing
+
+struct PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
